fcb23f1710ac2042fc0fe456a848c183d3f6f5f6
GregoSX <guilhermegrego376@gmail.com>
2023-08-31 17:03:30 -0300

    feat: Começo projeto Toughts

---
+++ 2_Toughts/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const flash = require('express-flash');
+
+const app = express();
+
+// Connect to DB
+const conn = require('./db/conn');
+
+// Handlebars Middleware - Template Engine
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Session Middleware
+app.use(
+    session({
+        name: 'session',
+        secret: 'nosso_secret',
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: () => {},
+            path: require('path').join(require('os').tmpdir(), 'sessions'),
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true,
+        }
+    }),
+)
+
+// Flash Middleware
+app.use(flash());
+
+// Public path
+app.use(express.static('public'));
+
+// Set session to res
+app.use((req, res, next) => {
+    if(req.session.userId) {
+        res.locals.session = req.session;
+    }
+
+    next();
+});
+
+const port = 3000;
+
+conn
+    .sync()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server running on port ${port}`);
+        });
+    }) 
+    .catch(err => console.log(err));
