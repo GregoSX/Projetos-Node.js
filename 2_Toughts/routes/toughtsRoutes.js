3a92467271c3f2d9282dc2e472334d0d650388f3
GregoSX <guilhermegrego376@gmail.com>
2023-09-01 09:55:21 -0300

    feat: middleware de autenticação

--- 2_Toughts/routes/toughtsRoutes.js
+++ 2_Toughts/routes/toughtsRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const ToughtController = require('../controllers/ToughtController');
 
+// Auth Middleware
+const checkAuth = require('../helpers/auth').checkAuth;
+
+router.get('/dashboard', checkAuth, ToughtController.showDashboard);
 router.get('/', ToughtController.showToughts);
 
 module.exports = router;
